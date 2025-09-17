aecc1f7056802e1f39a5cb44a65c77beb4e7f9a2
enuu <enuu9923@gmail.com>
2025-09-17 17:05:41 +0900

    9/17

--- script/script.js
+++ script/script.js
@@ -1,7 +1,55 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    aboutSlide();
+    reviewSlide();
+    spaceSlide();
 })
+// sec2
+const aboutSlide = () => {
+    var swiper = new Swiper(".aboutSwiper", {
+        loop:true,
+        spaceBetween: 0,
+        slidesPerView: 6,
+        centeredSlides: true,
+        speed: 3000,
+        autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+        }
+    });
+}
+// sec4
+const reviewSlide = () => {
+    var swiper = new Swiper(".reviewSwiper", {
+        loop:true,
+        spaceBetween: 10,
+        slidesPerView: 3,
+        centeredSlides: true,
+        speed: 3000,
+        // autoplay: {
+        // delay: 0,
+        // disableOnInteraction: false
+        // }
+    });
+}
+// sec5
+const spaceSlide = () => {
+    var swiper = new Swiper(".spaceSwiper", {
+        loop:true,
+        spaceBetween: 10,
+        slidesPerView: 1,
+        centeredSlides: true,
+        speed: 1000,
+        autoplay: {
+        delay: 500,
+        disableOnInteraction: false
+        },
+        navigation: {
+        nextEl: ".sec5_next",
+        prevEl: ".sec5_prev",
+      },
+    });
+}
 
 
